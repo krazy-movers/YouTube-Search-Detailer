bcd40e1726d99d957d8158e7b2222663f1fad23d
KeitaHidaka <keitinn.reader@gmail.com>
2023-01-21 20:14:24 +0900

    検索できるように画面を修正

--- lib/screens/search_result.dart
+++ lib/screens/search_result.dart
@@ -1,25 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_api/youtube_api.dart';
 
 class SearchResultPage extends StatefulWidget {
-  String searchWord;
+  String searchWord = "";
   SearchResultPage(this.searchWord, {super.key});
 
   @override
-  State<SearchResultPage> createState() => _SearchResultPageState();
+  // ignore: library_private_types_in_public_api
+  _SearchResultPageState createState() => _SearchResultPageState();
 }
 
 class _SearchResultPageState extends State<SearchResultPage> {
+  static String key = "{apikey}";
+
+  YoutubeAPI youtube = YoutubeAPI(key, maxResults: 15, type: 'video');
+  List<YouTubeVideo> videoResult = [];
+
+  Future<void> callAPI() async {
+    String query = widget.searchWord;
+    videoResult = await youtube.search(query);
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    callAPI();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue[100],
       appBar: AppBar(
-        title: const Text('検索結果'),
+        title: const Text('Youtube API'),
       ),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          children: [
-            Text(widget.searchWord),
+      body: ListView(
+        children: videoResult.map<Widget>(listItem).toList(),
+      ),
+    );
+  }
+
+  Widget listItem(YouTubeVideo video) {
+    return Card(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 7.0),
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Image.network(
+                video.thumbnail.small.url ?? '',
+                width: 120.0,
+              ),
+            ),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    video.title,
+                    softWrap: true,
+                    style: const TextStyle(fontSize: 18.0),
+                  ),
+                  Text(
+                    video.url,
+                    softWrap: true,
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
